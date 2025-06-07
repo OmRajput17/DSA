d870aa21d967b021e69b1d5c6fb12ad26866f7bc
Om Rajput <rajputom2408@gmail.com>
2025-06-07 20:35:02 +0530

    bubble-sort

--- Sorting/bubble-sort.cpp
+++ Sorting/bubble-sort.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     vector<int> bubbleSort(vector<int>& nums) {
         int n = nums.size(), temp;
+        int did_swap = 0;
         for(int i = n-1;i >= 1; i--){
             for(int j = 0; j <= i-1; j++){
                 if(nums[j] > nums[j+1]){
                     temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+
+                    did_swap = 1;
                 }
             }
+        }
+
+        if (did_swap == 0){
+            break;
         }
         return nums;
     }
