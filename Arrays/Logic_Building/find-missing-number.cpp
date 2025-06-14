8910ce275985bf5958ac0703076206b9fd0c2ac2
Om Rajput <rajputom2408@gmail.com>
2025-06-14 21:31:15 +0530

    finding-missing-number

--- Arrays/Logic_Building/find-missing-number.cpp
+++ Arrays/Logic_Building/find-missing-number.cpp
@@ -14,3 +14,23 @@ public:
 
 // Time Complexity:O(n) due to the accumulate function iterating through the nums vector, where n is the size of the input vector.
 // Space Complexity:O(1) as the algorithm uses a constant amount of extra space, regardless of the input size.
+
+class Solution {
+public:
+    // Function to find the missing number
+    int missingNumber(vector<int>& nums) {
+        int xor1 = 0, xor2 = 0;
+
+        // Calculate XOR of all array elements
+        for (int i = 0; i < nums.size(); i++) {
+            xor1 = xor1 ^ (i + 1); //XOR up to [1...N]
+            xor2 = xor2 ^ nums[i]; // XOR of array elements
+        }
+
+        // XOR of xor1 and xor2 gives missing number
+        return (xor1 ^ xor2);
+    }
+};
+
+// Time Complexity:O(n) due to the single for loop iterating through the input array.
+// Space Complexity:O(1) as only constant extra space is used for variables.
