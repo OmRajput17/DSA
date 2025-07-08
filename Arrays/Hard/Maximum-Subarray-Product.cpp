add5d3851e82bd514872c4b081c022863ee47b94
Om Rajput <rajputom2408@gmail.com>
2025-07-08 21:23:03 +0530

    Maximum Subarray Product

--- Arrays/Hard/Maximum-Subarray-Product.cpp
+++ Arrays/Hard/Maximum-Subarray-Product.cpp
@@ -15,3 +15,6 @@ public:
         return maxi;    
     }
 };
+
+// Time Complexity:The time complexity is O(n) because the code iterates through the input vector `nums` once.
+// Space Complexity:The space complexity is O(1) because the code uses a constant amount of extra space, regardless of the input size.
