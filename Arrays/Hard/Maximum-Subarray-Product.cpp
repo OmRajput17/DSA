7948006e8c6161f38f6f5183d59c231c5a411026
Om Rajput <rajputom2408@gmail.com>
2025-07-08 22:54:21 +0530

    Maximum Subarray Product

--- Arrays/Hard/Maximum-Subarray-Product.cpp
+++ Arrays/Hard/Maximum-Subarray-Product.cpp
@@ -18,3 +18,24 @@ public:
 
 // Time Complexity:The time complexity is O(n) because the code iterates through the input vector `nums` once.
 // Space Complexity:The space complexity is O(1) because the code uses a constant amount of extra space, regardless of the input size.
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int n = nums.size();
+        int pre = 1, suf = 1;
+
+        for(int i = 0; i<n; i++){
+            if (pre == 0) pre = 1;
+            if (suf == 0) suf = 1;
+
+            pre *= nums[i];
+            suf *= nums[n - 1 - i];
+
+            maxi = max(maxi ,max(pre, suf));
+        }
+
+        return maxi; 
+    }
+};
