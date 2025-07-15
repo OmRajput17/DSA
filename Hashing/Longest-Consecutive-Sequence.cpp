4f2ea0746a393e9d3419eeb9a51e0290096f5a84
Om Rajput <rajputom2408@gmail.com>
2025-07-15 21:00:03 +0530

    Longest Consecutive Subsequence

--- Hashing/Longest-Consecutive-Sequence.cpp
+++ Hashing/Longest-Consecutive-Sequence.cpp
@@ -3,9 +3,7 @@ class solution{
 private:
     int longestConsecutive(vector<int>& arr){
         int longest = 1;
-
         unordered_set<int> st;
-
         for(int i = 0; i<arr.size(); i++){
             st.insert(arr[i]);
         }
@@ -13,16 +11,13 @@ private:
             if(st.find(it-1) == st.end()){
                 int cnt = 1;
                 int x = it;
-                
                 while(st.find(x+1) != st.end()){
                     x = x + 1;
                     cnt += 1;
                 }
-
                 longest = max(longest, cnt);
             }
         }
-
         return longest;
     }
 };
