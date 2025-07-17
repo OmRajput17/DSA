330c60f88a1f09b167da3cb2e7cf26a17d302107
Om Rajput <rajputom2408@gmail.com>
2025-07-17 20:12:34 +0530

    Longest SubArray with Sum K

--- Hashing/Longest-SubArray-Sum-K.cpp
+++ Hashing/Longest-SubArray-Sum-K.cpp
@@ -1,5 +1,28 @@
 #include<bits/stdc++.h>
-
+// Optimal Solution for Positives
+class solution{
+public:
+    int longestSubarray(vector<int> nums, int k){
+        int left = 0, right = 0;
+        int sum = nums[0];
+        int maxLen = 0;
+        int n = nums.size();
+        while (right<n){
+            while(left<= right &&  sum>k){
+                sum -= nums[left];
+                left++;
+            }
+            if(sum == k){
+                maxLen = max(maxLen, right - left + 1);
+            }
+            right++;
+            if (right<n){
+                sum += nums[right];
+            }
+        }
+        return maxLen;
+    }
+};
 
 // Optimal Solution for Negative and Positives
 class solution{
