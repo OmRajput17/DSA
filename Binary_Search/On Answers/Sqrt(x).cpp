b9138191785ae503de6512508db076b6ac7dcdbc
Om Rajput <rajputom2408@gmail.com>
2025-08-04 20:19:10 +0530

    SQRT

--- Binary_Search/On Answers/Sqrt(x).cpp
+++ Binary_Search/On Answers/Sqrt(x).cpp
@@ -2,6 +2,26 @@
 
 class Solution {
 public:
+    long long floorSqrt(long long x) {
+        long long low = 1, high = x;
+        int ans = 0;
+        while(low<=high){
+            long long mid = (low+high)/2;
+
+            if(mid*mid <= x){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
     int mySqrt(int x) {
         int low = 0, high = x;
         while(low<=high){
