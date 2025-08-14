4c1cceff12a62b86476f83c950d618e38ffb59e8
Om Rajput <rajputom2408@gmail.com>
2025-08-14 23:02:45 +0530

    LC POTD

---
+++ String/Largest-3-Same-Digit-Number-String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string largest = "";
+        for(int i = 1; i<num.size()-1; i++){
+            if(num[i]==num[i-1] && num[i]==num[i+1]){
+                string curr(3, num[i]);
+                if(curr>largest){
+                    largest = curr;
+                }
+            }
+        }
+        return largest;
+    }
+};
